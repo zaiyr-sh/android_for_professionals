396bb1ca726a19ed6ea1573dbba04384fa79b007
Zaiyr <zaiyr.00@gmail.com>
2021-07-26 19:49:46 +0600

    chapter 4

--- Project_1/GeoQuiz/app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ Project_1/GeoQuiz/app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -8,8 +8,10 @@ import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,21 +22,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nextButton: ImageButton
     private lateinit var questionTextView: TextView
     private lateinit var numberCorrectAnswerTextView: TextView
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
-    )
-    private var currentIndex = 0
     private var countCorrectAnswer = 0
+
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProvider(this).get(QuizViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "Got a QuizViewModel: $quizViewModel")
+
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
+        quizViewModel.currentIndex = currentIndex
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -57,29 +58,14 @@ class MainActivity : AppCompatActivity() {
             isChoosingButtonsClickable(false)
         }
 
-        prevButton.setOnClickListener {
-            if (currentIndex == 0) {
-                currentIndex = questionBank.lastIndex
-            }
-            currentIndex = (currentIndex - 1) % questionBank.size
-            updateQuestion()
-            isChoosingButtonsClickable(false)
-        }
-
         nextButton.setOnClickListener {
-            if (currentIndex + 1 == questionBank.size) {
-                numberCorrectAnswerTextView.text =
-                    resources.getString(R.string.number_correct_answer, countCorrectAnswer)
-            } else {
-                numberCorrectAnswerTextView.text = ""
-            }
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
             isChoosingButtonsClickable(true)
         }
 
         questionTextView.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
         }
 
@@ -101,6 +87,12 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onPause() called")
     }
 
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG, "onSaveInstanceState")
+        savedInstanceState.putInt(KEY_INDEX, quizViewModel.currentIndex)
+    }
+
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop() called")
@@ -112,12 +104,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
 
         val messageResId = if (userAnswer == correctAnswer) {
             countCorrectAnswer++
